548db591d9ea2f750e39604048546dd7ee8a0f4d
SiriChandhana06 <sirisiri3006@gmail.com>
2024-11-18 19:58:29 +0530

    Light & Dark Mode Icon functionality

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { IoSunnyOutline } from "react-icons/io5";
+import { IoMoonOutline } from "react-icons/io5";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [userEmail, setUserEmail] = useState('');
     const [showLogout, setShowLogout] = useState(false);
     const dropdownRef = useRef(null);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -15,6 +18,19 @@ const Navbar = () => {
     const toggleLogout = () => {
         setShowLogout(!showLogout);
     };
+    // const toggleTheme = () => {
+    //     setIsDarkMode(!isDarkMode); 
+    //   };
+
+    const toggleTheme = () => {
+        setIsDarkMode(!isDarkMode);
+        if (isDarkMode) {
+          document.documentElement.classList.add("dark"); 
+        } else {
+          document.documentElement.classList.remove("dark"); 
+        }
+      };
+      
 
     const handleLogout = () => {
         localStorage.removeItem('userEmail');
@@ -43,6 +59,13 @@ const Navbar = () => {
         }
     }, []);
 
+    useEffect(() => {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          document.documentElement.classList.add("dark");
+          setIsDarkMode(true);
+        }
+      }, []);
+
 
     return (
         <nav className="bg-[#303030] p-4 rounded-full flex justify-between items-center mx-4 lg:mx-32 my-4">
@@ -56,6 +79,10 @@ const Navbar = () => {
                 <Link to="/visualizer" className="hover:text-gray-400">Visualizer</Link>
                 <Link to="/portfoliotracker" className="hover:text-gray-400">Portfolio Tracker</Link>
             </div>
+            {/* <div className='text-white flex justify-end'>
+            <IoSunnyOutline />
+            <IoMoonOutline />
+            </div> */}
             {userEmail ? (
                 <div className="relative hidden md:flex" ref={dropdownRef}>
                     <button onClick={toggleLogout} className="hidden md:flex bg-white text-black rounded-full px-4 py-2">{userEmail}</button>
@@ -71,15 +98,30 @@ const Navbar = () => {
                     )}
                 </div>
             ) : (
-                <Link to='/loginpage'>
-                    <button className="hidden md:flex bg-white text-black rounded-full px-4 py-2">
-                        Login
-                    </button>
-                </Link>
+                <div className='hidden md:flex justify-center gap-4'>
+                    <div className='text-white text-2xl mt-2 cursor-pointer' onClick={toggleTheme}>
+                        {/* <IoSunnyOutline /> */}
+                        {/* <IoMoonOutline /> */}
+                        {isDarkMode ? <IoMoonOutline /> : <IoSunnyOutline />}
+                    </div>
+                    <Link to='/loginpage'>
+                        <button className="hidden md:flex bg-white text-black rounded-full px-4 py-2">
+                            Login
+                        </button>
+                    </Link>
+                </div>
             )}
 
-            <div className="md:hidden text-white" onClick={toggleMenu}>
-                {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            <div className="md:hidden flex justify-center gap-2 text-white">
+                <div className='text-white text-2xl cursor-pointer' onClick={toggleTheme}>
+                    {/* <IoSunnyOutline /> */}
+                    {/* <IoMoonOutline /> */}
+                    {isDarkMode ? <IoMoonOutline /> : <IoSunnyOutline />}
+
+                </div>
+                <div onClick={toggleMenu}>
+                    {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+                </div>
             </div>
 
             {isOpen && (
