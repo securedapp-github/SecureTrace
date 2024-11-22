e8b11501abd5b383521b4ab07eafd06ccf599d8b
SiriChandhana06 <sirisiri3006@gmail.com>
2024-11-22 20:49:13 +0530

    Visualizer Update

--- src/Components/Visualizer.jsx
+++ src/Components/Visualizer.jsx
@@ -76,7 +76,7 @@ const Visualizer = () => {
         setTransfers(combinedTransfers);
         renderGraph(value || inputValue, combinedTransfers);
         setValidationMessage('Valid wallet address found!');
-        if (inputValue.length > 0) {
+        if (inputValue.length > 0 || value.length > 0) {
           setIsInputEntered(true);
         } else {
           setIsInputEntered(false);
@@ -103,8 +103,8 @@ const Visualizer = () => {
         console.log(response.data);
 
         setTransfers(response.data.transfers);
-        // renderGraphTxHash();
-        setValidationMessage('Valid wallet address found!');
+        renderGraphTxHash(inputValue||value, response.data.transfers);
+        setValidationMessage('Valid tx found!');
         if (inputValue.length > 0) {
           setIsInputEntered(true);
         } else {
@@ -117,7 +117,7 @@ const Visualizer = () => {
       setLoading(false);
 
     } else {
-      setValidationMessage('Invalid input. Please enter a valid wallet address.');
+      setValidationMessage('Invalid input. Please enter a valid wallet address or tx hash.');
     }
   };
 
@@ -452,200 +452,190 @@ const Visualizer = () => {
 
 
 
-  function dragged(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
-  }
-
-  function dragged(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
-  }
-
-
-
-  // const renderGraphTxHash = () => {
-  //   const nodes = [{ id: centerAddress, center: true }];
-  //   const links = [];
-
-  //   transactions.forEach((tx) => {
-  //     const isIncoming = tx.to.toLowerCase() === centerAddress.toLowerCase();
-  //     const target = isIncoming ? tx.from : tx.to;
-
-  //     // Ensure the node for each transaction party exists
-  //     if (!nodes.some(node => node.id === target)) {
-  //       nodes.push({ id: target, type: isIncoming ? 'incoming' : 'outgoing' });
-  //     }
-
-  //     links.push({
-  //       source: centerAddress,
-  //       target,
-  //       blockNum: tx.blockNum,
-  //       type: isIncoming ? 'incoming' : 'outgoing',
-  //     });
-  //   });
-
-  //   d3.select(svgRef.current).selectAll('*').remove();
-
-  //   const width = 2000;
-  //   const height = 1000;
-
-  //   const simulation = d3
-  //     .forceSimulation(nodes)
-  //     .force('link', d3.forceLink(links).id((d) => d.id).distance(300))
-  //     .force('charge', d3.forceManyBody().strength(-600))
-  //     .force('center', d3.forceCenter(width / 2, height / 2))
-  //     .force('x', d3.forceX((d) => d.center ? width / 2 : d.type === 'incoming' ? width / 3 : 2 * width / 3))
-  //     .force('y', d3.forceY(height / 2).strength(0.05));
-
-  //   const svg = d3
-  //     .select(svgRef.current)
-  //     .attr('width', width)
-  //     .attr('height', height);
-
-  //   const linkGroups = d3.group(links, d => `${d.source}-${d.target}`);
-
-  //   const generatePathData = (d, i, total) => {
-  //     const dx = d.target.x - d.source.x;
-  //     const dy = d.target.y - d.source.y;
-  //     const dr = Math.sqrt(dx * dx + dy * dy);
-  //     const curveOffset = (i - (total - 1) / 2) * 20; // Adjust the curve offset
-  //     return `M${d.source.x},${d.source.y}A${dr},${dr} 0 0,${i % 2 === 0 ? 1 : 0} ${d.target.x},${d.target.y}`;
-  //   };
-  //   // Links
-  //   const link = svg
-  //     .append('g')
-  //     .attr('class', 'links')
-  //     .selectAll('path')
-  //     .data(links)
-  //     .enter()
-  //     .append('path')
-  //     .attr('stroke', (d) => (d.type === 'incoming' ? 'green' : 'red'))
-  //     .attr('stroke-width', 1)
-  //     .attr('fill', 'none')
-  //     .attr('d', (d, i) => {
-  //       const group = linkGroups.get(`${d.source}-${d.target}`);
-  //       return group.length > 1 ? generatePathData(d, i, group.length) : `M${d.source.x},${d.source.y}L${d.target.x},${d.target.y}`;
-  //     })
-  //     .on('mouseover', function (event, d) {
-  //       d3.select(this).attr('stroke', '#555');
-  //       tooltip
-  //         .style('opacity', 1)
-  //         .html(`Hash: ${d.hash}`)
-  //         .style('left', `${event.pageX + 10}px`)
-  //         .style('top', `${event.pageY + 10}px`);
-  //     })
-  //     .on('mouseout', function () {
-  //       d3.select(this).attr('stroke', (d) => (d.type === 'incoming' ? 'green' : 'red'));
-  //       tooltip.style('opacity', 0);
-  //     })
-  //     .on('click', function (event, d) {
-  //       console.log(d);
-  //       handleLinkClick(d.target.id, d.blockNum, d.type === 'incoming' ? false : true);
-  //     });
-
-  //   // Nodes
-  //   const node = svg
-  //     .append('g')
-  //     .attr('class', 'nodes')
-  //     .selectAll('circle')
-  //     .data(nodes)
-  //     .enter()
-  //     .append('circle')
-  //     .attr('r', (d) => (d.center ? 35 : 30))
-  //     .attr('fill', (d) => (d.center ? '#d6b4fc' : '#40a9f3'))  // Blue nodes for all transactions
-  //     .call(drag(simulation))
-  //     .on('mouseover', function (event, d) {
-  //       d3.select(this).attr('fill', (d) => (d.center ? '#d6b4fc' : '#40a9f3'));
-  //       tooltip
-  //         .style('opacity', 1)
-  //         .html(`Address: ${d.id}`)
-  //         .style('left', `${event.pageX + 10}px`)
-  //         .style('top', `${event.pageY + 10}px`);
-  //     })
-  //     .on('mouseout', function () {
-  //       d3.select(this).attr('fill', (d) => (d.center ? '#d6b4fc' : '#40a9f3'));
-  //       tooltip.style('opacity', 0);
-  //     });
-
-  //   const text = svg
-  //     .append('g')
-  //     .attr('class', 'labels')
-  //     .selectAll('text')
-  //     .data(nodes)
-  //     .enter()
-  //     .append('text')
-  //     .attr('dy', 5)
-  //     .attr('dx', -25)
-  //     .style('font-size', '12px')
-  //     .text((d) => d.id.substring(0, 6) + '...');
-
-  //   const tooltip = d3
-  //     .select('body')
-  //     .append('div')
-  //     .attr('class', 'tooltip')
-  //     .style('position', 'absolute')
-  //     .style('text-align', 'left')
-  //     .style('width', '500px')
-  //     .style('padding', '8px')
-  //     .style('font', '12px sans-serif')
-  //     .style('background', 'lightsteelblue')
-  //     .style('border', '1px solid #fff')
-  //     .style('border-radius', '8px')
-  //     .style('pointer-events', 'none')
-  //     .style('opacity', 0);
-
-  //   const showTooltip = (content, x, y) => {
-  //     tooltip
-  //       .html(content)
-  //       .style('left', `${x}px`)
-  //       .style('top', `${y}px`)
-  //       .style('opacity', 1);
-  //   };
-
-  //   const hideTooltip = () => {
-  //     tooltip.style('opacity', 0);
-  //   };
-
-  //   node.on('mouseover', function (event, d) {
-  //     showTooltip(`Address: ${d.id}`, event.pageX + 10, event.pageY + 10);
-  //   });
-
-  //   document.addEventListener('click', (event) => {
-  //     const isTooltipClick = tooltip.node().contains(event.target);
-  //     if (!isTooltipClick) hideTooltip();
-  //   });
-
-  //   simulation.on('tick', () => {
-  //     link.attr('d', (d, i) => {
-  //       const group = linkGroups.get(`${d.source}-${d.target}`);
-  //       return group.length > 1 ? generatePathData(d, i, group.length) : `M${d.source.x},${d.source.y}L${d.target.x},${d.target.y}`;
-  //     });
-  //     node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
-  //     text.attr('x', (d) => d.x).attr('y', (d) => d.y);
-  //   });
-
-  //   function drag(simulation) {
-  //     function dragstarted(event, d) {
-  //       if (!event.active) simulation.alphaTarget(0.3).restart();
-  //       d.fx = d.x;
-  //       d.fy = d.y;
-  //     }
-
-  //     function dragged(event, d) {
-  //       d.fx = event.x;
-  //       d.fy = event.y;
-  //     }
-
-  //     function dragended(event, d) {
-  //       if (!event.active) simulation.alphaTarget(0);
-  //       d.fx = null;
-  //       d.fy = null;
-  //     }
-
-  //     return d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended);
-  //   }
+  // function dragged(event, d) {
+  //   d.fx = event.x;
+  //   d.fy = event.y;
   // }
+
+  // function dragged(event, d) {
+  //   d.fx = event.x;
+  //   d.fy = event.y;
+  // }
+
+
+
+  const renderGraphTxHash = (inputTxHash, transactions) => {
+    const elements = [];
+    
+    const shortenAddress = (address) => `${address.slice(0, 5)}...${address.slice(-4)}`;
+  
+    transactions.forEach((tx) => {
+      const source = tx.from;
+      const target = tx.to;
+  
+      if (!elements.some((el) => el.data.id === source)) {
+        elements.push({
+          data: { 
+            id: source,
+            label: shortenAddress(source),
+            chain: tx.chain,
+            blockNum: tx.blockNum,
+            value: tx.value * tx.tokenPrice,
+          },
+          classes: 'node',
+        });
+      }
+  
+      if (!elements.some((el) => el.data.id === target)) {
+        elements.push({
+          data: { 
+            id: target,
+            label: shortenAddress(target),
+            chain: tx.chain,
+            blockNum: tx.blockNum,
+            value: tx.value * tx.tokenPrice,
+          },
+          classes: 'node',
+        });
+      }
+  
+      elements.push({
+        data: {
+          source: source,
+          target: target,
+          hoverLabel: inputTxHash,
+          chain: tx.chain,
+          blockNum: tx.blockNum,
+          value: tx.value * tx.tokenPrice,
+          label: tx.value * tx.tokenPrice,
+        },
+        classes: tx.type === 'incoming' ? 'incoming-edge' : 'outgoing-edge',
+      });
+    });
+  
+    const cy = cytoscape({
+      container: document.getElementById('cy'), // HTML element to attach the graph
+  
+      elements: elements,
+  
+      style: [
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#40a9f3',
+            'label': 'data(label)',
+            'width': 50,
+            'height': 50,
+            'text-valign': 'center',
+            'color': '#fff',
+            'font-size': '10px',
+          },
+        },
+        {
+          selector: '.center-node',
+          style: {
+            'background-color': '#d6b4fc',
+            'width': 70,
+            'height': 70,
+          },
+        },
+        {
+          selector: 'edge',
+          style: {
+            'width': 1,
+            'line-color': '#ddd',
+            'curve-style': 'bezier',
+            'label': 'data(label)',
+            'font-size': '8px',
+            'text-outline-width': 1,
+            'text-outline-color': '#fff',
+          },
+        },
+        {
+          selector: '.incoming-edge',
+          style: {
+            'line-color': 'green',
+            'source-arrow-color': 'green',
+            'source-arrow-shape': 'triangle',
+          },
+        },
+        {
+          selector: '.outgoing-edge',
+          style: {
+            'line-color': 'red',
+            'target-arrow-color': 'red',
+            'target-arrow-shape': 'triangle',
+          },
+        },
+      ],
+  
+      layout: {
+        name: 'cose',
+        padding: 10,
+        nodeRepulsion: 5000,
+        idealEdgeLength: 50,
+        edgeElasticity: 100,
+      },
+      wheelSensitivity: 0.2,
+    });
+  
+    const tooltip = document.createElement('div');
+    tooltip.style.position = 'absolute';
+    tooltip.style.padding = '8px';
+    tooltip.style.background = 'lightsteelblue';
+    tooltip.style.border = '1px solid #fff';
+    tooltip.style.borderRadius = '8px';
+    tooltip.style.pointerEvents = 'none';
+    tooltip.style.opacity = 0;
+    document.body.appendChild(tooltip);
+  
+    // Function to show the tooltip
+    const showTooltip = (content, x, y) => {
+      tooltip.innerHTML = content.replace(/\n/g, '<br>'); // Replace \n with <br>
+      tooltip.style.left = `${x + 10}px`; // Adjust the x position
+      tooltip.style.top = `${y + 10}px`; // Adjust the y position
+      tooltip.style.opacity = 1;
+    };
+  
+    // Function to hide the tooltip
+    const hideTooltip = () => {
+      tooltip.style.opacity = 0;
+    };
+  
+    // Attach event listeners for tooltip-like behavior
+    cy.on('mouseover', 'node', (event) => {
+      const nodeData = event.target.data();
+      const { x, y } = event.renderedPosition;
+      showTooltip(`Address: ${nodeData.id}`, x, y);
+    });
+  
+    cy.on('mouseout', 'node', () => {
+      hideTooltip();
+    });
+  
+    cy.on('mouseover', 'edge', (event) => {
+      const edgeData = event.target.data();
+      const { x, y } = event.renderedPosition;
+      showTooltip(`Transaction Hash: ${edgeData.hoverLabel}<br>Value: $${parseFloat(edgeData.value).toFixed(2)}`, x, y);
+    });
+  
+    cy.on('mouseout', 'edge', () => {
+      hideTooltip();
+    });
+  
+    cy.on('tap', 'node', (event) => {
+      const nodeData = event.target.data();
+      handleLinkClick(nodeData.id, nodeData.blockNum, nodeData.type, nodeData.chain);
+    });
+  
+    cy.layout({
+      name: 'cose',
+      padding: 10,
+      nodeRepulsion: 5000,
+      idealEdgeLength: 50,
+      edgeElasticity: 100,
+    }).run();
+  };
 
 
   return (
