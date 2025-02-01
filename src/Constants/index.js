5e41fe11a5297c2107d0b04c9351cc9d2763c17b
Gokkull04 <gokkull04@gmail.com>
2025-02-01 21:32:14 +0530

    html2canvan to dom2image

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,2 +1,2 @@
 export const DevUrl = "https://139-59-5-56.nip.io:3443";
- //export const DevUrl = "https://caiman-wanted-fox.ngrok-free.app";
+//export const DevUrl = "https://caiman-wanted-fox.ngrok-free.app";
