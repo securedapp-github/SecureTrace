d2b52c8552319bb4b7874366568e5f1f113f9625
Gokkull04 <gokkull04@gmail.com>
2025-02-04 12:07:34 +0530

    portfolio tracker transfers logo fixes

--- src/Components/AddressCard.jsx
+++ src/Components/AddressCard.jsx
@@ -832,17 +832,26 @@ const AddressCard = () => {
                             tokenPrice,
                             chain,
                           } = transfer;
+
+                          // Determine the logo to display
+                          const displayLogo =
+                            transfer.logo === "https://algorand.org/logo.png"
+                              ? algo
+                              : transfer.logo;
+
                           return (
                             <tr
                               key={index}
                               className="border-t  h-12  text-center bg-red-600 odd:bg-[#F4F4F4] even:bg-white px-2 py-2"
                             >
                               <td className="flex items-center justify-center px-4 mt-2">
-                                <img
-                                  src={logo}
-                                  alt={tokenName}
-                                  className="h-9"
-                                />
+                                {displayLogo && ( // Only render the image if displayLogo is not null
+                                  <img
+                                    src={displayLogo}
+                                    alt={tokenName}
+                                    className="rounded-full h-9"
+                                  />
+                                )}
                               </td>
                               {/* <td className="px-4 text-green-500 me-3">{timestamp}</td> */}
                               <td className="px-4 text-green-500 me-3">
